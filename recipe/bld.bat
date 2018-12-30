85f0964d42a3633d7b4a9e00df92b02e4a9c39cf
Patrick Sodré <psodre@gmail.com>
2018-12-30 21:00:46 +0000

    try to converge bld.bat and build.sh

--- recipe/bld.bat
+++
@@ -1,9 +0,0 @@
-copy %RECIPE_DIR%\build.sh build.sh
-
-set GOPATH=%SRC_DIR%
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-bash build.sh
-if errorlevel 1 exit 1
-exit 0
