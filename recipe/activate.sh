d325f992a006846aba2eca2cf796d56b154ff47e
Stuart Berg <bergs@janelia.hhmi.org>
2020-04-21 17:37:48 -0400

    activate.sh: Use alias -L in zsh, alias -p in bash

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,3 +1,7 @@
-export CF_ALIAS_GIT_BACKUP=$(alias -p | grep git=)
+if [ -n "$ZSH_VERSION" ]; then
+    export CF_ALIAS_GIT_BACKUP=$(alias -L | grep ' git=')
+else
+    export CF_ALIAS_GIT_BACKUP=$(alias -p | grep  'git=')
+fi
 
 alias git=$(which hub)
--- recipe/deactivate.sh
+++ recipe/deactivate.sh
@@ -1,3 +1,8 @@
-alias -p | grep -c git= >/dev/null && unalias git
+if [ -n "$ZSH_VERSION" ]; then
+    alias -p | grep -c git= >/dev/null && unalias git
+else
+    alias -L | grep -c git= >/dev/null && unalias git
+fi
+
 eval $CF_ALIAS_GIT_BACKUP
 unset CF_ALIAS_GIT_BACKUP
