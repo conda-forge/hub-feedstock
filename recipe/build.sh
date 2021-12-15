91e380c0328caa91ae1c14aa65a079f783d157f9
ngam <67342040+ngam@users.noreply.github.com>
2021-12-15 13:45:31 -0500

    add osxarm support

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,12 @@ set -eux
 mkdir -p $PREFIX/etc/bash_completion.d;
 cp etc/hub.bash_completion.sh $PREFIX/etc/bash_completion.d/hub.sh
 
-go install -v .
+if [[ "$target_platform" == "osx-arm64" ]]; then  
+  go build -v .
+  mkdir -p ${PREFIX}/bin
+  install hub ${PREFIX}/bin
+fi
+
+if [[ "$target_platform" != "osx-arm64" ]]; then
+  go install -v .
+fi
